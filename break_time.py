b18476df61ca2f28d2a2c51bd1c9beea20c221e0
Gihan Karunarathne <gckarunarathne@gmail.com>
2015-04-22 23:46:36 +0530

    Repeat open url 3 times

--- break_time.py
+++ break_time.py
@@ -1,5 +1,11 @@
 import time
 import webbrowser
 
-time.sleep(5)
-webbrowser.open("https://www.youtube.com/watch?v=j9g3uUsQi2s")
+totalBreaks = 3
+breakCount = 0
+
+print ("This program started on " + time.ctime())
+while(breakCount < totalBreaks):
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=j9g3uUsQi2s")
+    breakCount += 1
