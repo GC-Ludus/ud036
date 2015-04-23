34669a8b7dafae4798f224ad3ffc8365304dbe99
Gihan Karunarathne <gckarunarathne@gmail.com>
2015-04-24 01:29:36 +0530

    Draw a square

--- mindstorms.py
+++ mindstorms.py
@@ -5,8 +5,12 @@ def drawSquare():
     window.bgcolor('red')
 
     ibba = turtle.Turtle()
-    ibba .forward(200)
-
+    ibba.forward(200)
+    steps = 3
+    for x in range(0, steps):
+        ibba.right(90)
+        ibba.forward(200)
+    
     window.exitonclick()
 
 drawSquare()
