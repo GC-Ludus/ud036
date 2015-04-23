65222ed6f0defa4241ed610fee8c6e38fc431c6d
Gihan Karunarathne <gckarunarathne@gmail.com>
2015-04-24 01:20:44 +0530

    Draw a line on a screen window

---
+++ mindstorms.py
@@ -0,0 +1,12 @@
+import turtle
+
+def drawSquare():
+    window = turtle.Screen()
+    window.bgcolor('red')
+
+    ibba = turtle.Turtle()
+    ibba .forward(200)
+
+    window.exitonclick()
+
+drawSquare()
