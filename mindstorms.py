afdabce1d4b3876c27235cfe683627b29fb3c6fb
Gihan Karunarathne <gckarunarathne@gmail.com>
2015-04-24 09:17:18 +0530

    Make code reusable
    * Implement drawTriangle method

--- mindstorms.py
+++ mindstorms.py
@@ -1,20 +1,30 @@
 import turtle
 
 def drawSquare():
-    window = turtle.Screen()
-    window.bgcolor('red')
-
     ibba = turtle.Turtle()
     ibba.shape('circle')
     ibba.color('green')
     ibba.speed(2.5)
     
-    ibba.forward(200)
+    steps = 4
+    for x in range(0, steps):
+        ibba.forward(100)
+        ibba.right(90)
+    
+def drawCircle():
+    ibba = turtle.Turtle()
+    ibba.circle(50)
+    
+def drawTriangle():
+    ibba = turtle.Turtle()
     steps = 3
     for x in range(0, steps):
-        ibba.right(90)
-        ibba.forward(200)
+        ibba.forward(100)
+        ibba.right(120)
     
-    window.exitonclick()
-
+window = turtle.Screen()
+window.bgcolor('red')
 drawSquare()
+drawCircle()
+drawTriangle()
+window.exitonclick()
